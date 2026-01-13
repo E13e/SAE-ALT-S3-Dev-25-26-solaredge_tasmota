de57012b85dec9996fa85ebe15a54a21b66c2595
GuillaumeLarroutis <guillaume.larroutisorange.com@gmail.com>
2026-01-13 19:54:41 +0100

    Ajout release && Compte rendu Sprint1

---
+++ release_Sprint1/README.txt
@@ -0,0 +1,18 @@
+# Sprint 1
+
+## Fonctionnalités implementées
+
+Il est possible de lancer le projet docker à l'aide des commandes présentes dans le fichier [cmd_docker.txt](https://github.com/E13e/SAE-ALT-S3-Dev-25-26-solaredge_tasmota/blob/main/cmd_docker.txt) (présent dans le zip). /!\ Ces commandes doivent être entrée dans le répertoire docker /!\. Une fois lancée les données des panneaux solaires sont accessibles via ce lien http://127.0.0.1:8080/php/.
+
+
+Si vous avez le moindre soucis, vous pouvez aller voir dans la [documentation utilisateur](https://github.com/E13e/SAE-ALT-S3-Dev-25-26-solaredge_tasmota/blob/main/gpo/docs/doc_utilisateur.adoc) ou encore, dans une moindre mesure, [le cahier de test](https://github.com/E13e/SAE-ALT-S3-Dev-25-26-solaredge_tasmota/blob/main/gpo/docs/carnet_test.adoc).
+
+## En cas de problème
+
+Vous pouvez contacter une de ces trois adresses mail :
+
+    - guillaume.larroutis@etu.univ-tlse2.fr
+
+    - jeremy.ratsimba@etu.univ-tlse2.fr
+
+    - alexis.dupont@etu.univ-tlse2.fr
---
+++ release_Sprint1/docker/php/index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <title>Dashboard SAE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body onload="setInterval('window.location.reload()', 60000);">
+
+
+    <h1>cc</h1>
+
+    <?php 
+        $fichier = "/var/www/json/pageweb.json";
+        
+
+
+        if (file_exists($fichier)) {
+            $handler = fopen($fichier,"r");
+            $donnees = fread($handler, filesize($fichier));
+
+            fclose($handler);
+            
+            $donneesJson= json_decode($donnees, true);
+        } else {
+            $fichier = "../json/pageweb.json";
+
+            $handler = fopen($fichier,"r");
+            $donnees = fread($handler, filesize($fichier));
+            $donneesJson = json_decode($donnees, true);
+        }
+    ?>
+
+    <table>
+        <thead>
+            <?php if (!empty($donneesJson)): ?>
+                <?php foreach ($donneesJson as $key => $value): ?>
+                    <th><?php echo htmlspecialchars($key); ?></th>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </thead>
+        <tbody>
+            <?php if (!empty($donneesJson)): ?>
+                <tr>
+                    <?php foreach ($donneesJson as $key => $value): ?>
+                        <td>
+                            <?php 
+                            if (is_array($value)) {
+                                foreach ($value as $subKey => $subValue) {
+                                    echo $subKey . ": " . $subValue . "<br>";
+                                }
+                            } else {
+                                echo htmlspecialchars($value);
+                            }
+                            ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+
+</body>
+</html>
